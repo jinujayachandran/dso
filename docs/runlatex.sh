228d430796809d9ffa3943a901f6fe0198577123
jinujayachandran <jinujayachandran@gmail.com>
2017-12-05 19:46:42 +0530

    Documents created for dso

---
+++ docs/runlatex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+filename=$(find -iname $1.tex)
+filepath=$(dirname "${filename}")
+echo $filename
+echo $filepath
+cd $filepath
+latex $1.tex
+bibtex $1
+latex $1.tex
+latex $1.tex
+dvips -o $1.dvi
+dvipdfm $1.dvi
+evince $1.pdf &
+
